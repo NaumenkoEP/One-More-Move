27b5947f565d6ae2542c7a4e90437e3ff4dc10c2
Egor Naumenko <egornaumenko2007@gmail.com>
2026-01-27 13:55:28 +0200

    complete ios build (no ad integration just yet)

---
+++ www/hapticsModule.js
@@ -0,0 +1,25 @@
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+
+const hapticsImpactMedium = async () => {
+  await Haptics.impact({ style: ImpactStyle.Medium });
+};
+
+const hapticsImpactLight = async () => {
+  await Haptics.impact({ style: ImpactStyle.Light });
+};
+
+const hapticsVibrate = async () => {
+  await Haptics.vibrate();
+};
+
+const hapticsSelectionStart = async () => {
+  await Haptics.selectionStart();
+};
+
+const hapticsSelectionChanged = async () => {
+  await Haptics.selectionChanged();
+};
+
+const hapticsSelectionEnd = async () => {
+  await Haptics.selectionEnd();
+};
